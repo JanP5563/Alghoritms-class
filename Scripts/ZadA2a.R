1e89f22f626c9f745fcd2ec6f9b03bae566c97e8
Tomasz Żołądek <s230998@sggw.edu.pl>
2026-05-23 19:33:58 +0200

    Add part of exA2

--- Scripts/ZadA2a.R
+++ Scripts/ZadA2a.R
@@ -4,9 +4,40 @@ setwd("C:\\Users\\rysio\\Desktop\\Informatyka\\R\\DataMining\\Project\\Plots")
 x <- c(0.1, 0.5, 1, 1.5, 2.05, 2.5, 3)
 y <- c(0.23, 0.65, 0.9, 0.8, 0.7, 0.6, 0.5)
 
+model1 <- nls(y ~ (d * x) / (1 + e * x^2), 
+             start = list(d = 1, e = 1))
+summary(model1)
 
+model2 <- nls(y ~ (d* x) * exp(-b * x), 
+              start = list(b = 0.1, d = 1))
+summary(model2)
 
-png('plot1Ba.png', width = 1500, height = 1500, res = 150, type = "cairo", bg="white")
+model3 <- nls(y ~ a + b*x + c*x^2 + d*x^3, 
+              start = list(a = 1, b = 1, c = 1, d = 1))
+summary(model3)
+
 # ---------- Plot -------------------
+png('plotA2a.png', width = 1500, height = 1500, res = 150, type = "cairo", bg="white")
+
+plot(x, y, pch = 19, cex = 1.2, xlab = "x", ylab = "y", col = "#222222")
+grid(nx = NULL, ny = NULL, col = "lightgray", lty = "solid")
+# graph1
+cf <- coef(model1)
+curve((cf["d"] * x) / (1 + cf["e"] * x^2),
+      from = min(x), to = max(x),
+      add = TRUE, col = "#0072B2", lwd = 2, n = 500)
+# graph2
+cf <- coef(model2)
+curve((cf["d"] * x) * exp(-cf["b"] * x),
+      from = min(x), to = max(x),
+      add = TRUE, col = "#D55E00", lwd = 2, n = 500)
+# graph3
+cf <- coef(model3)
+curve(cf["a"] + cf["b"]*x + cf["c"]*x^2 + cf["d"]*x^3,
+      from = min(x), to = max(x),
+      add = TRUE, col = "#009E73", lwd = 2, n = 500)
+
+# points
+points(x, y, col = "#222222", pch = 19, cex = 1.2)
 
 dev.off()
